7fca8bf2912d6c9b277227a6e3da741c25176083
phybhavya <49296981+phybhavya@users.noreply.github.com>
2023-04-07 00:43:16 +0530

    Update ESP_now_master_controller.ino
    6th april Testing done,left and right rotate integrated

--- ESP_now_master_controller.ino
+++ ESP_now_master_controller.ino
@@ -35,42 +35,44 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
-
-
 // Global copy of slave
 esp_now_peer_info_t slave;
+#define M_PI 3.14
 #define CHANNEL 1
 #define PRINTSCANRESULTS 0
 #define DELETEBEFOREPAIR 0
 typedef struct message{
   float buton;
-//  float backward;
-//  float left;
-//  float right;
   float w0;
   float w1;
   float w2;
-  int mode;
+  float w3;
+  float con_mode;
   } message;
   message but_values;
   message joy_values;
 float speedd=100;
-int wheel_angle[3]={0, 120, 240};
+int wheel_angles[4]={135, 225,315,45};
 int i, j, k;
-int buttons[4] = {25,33,32,26};
-float vel_x, vel_y;
-float x_joy,y_joy;
-int joy_x =13 ;
-int joy_y =12 ;
+int buttons[6] = {25,33,32,26,34,27};
+int but_mode[2] = {18,19};
+float vel_x;
+float vel_y;
+int pot_x =  36; 
+int pot_y = 39;
 // Init ESP Now with fallback
-float vel[3] = {0,0,0};
+float vel[4] = {0,0,0,0};
+float offset_x = 46;
+float offset_y = -40;
+
+float x=0, y=0;
 void InitESPNow() {
   WiFi.disconnect();
   if (esp_now_init() == ESP_OK) {
-    Serial.println("ESPNow Init Success");
+   // Serial.println("ESPNow Init Success");
   }
   else {
-    Serial.println("ESPNow Init Failed");
+    //Serial.println("ESPNow Init Failed");
     // Retry InitESPNow, add a counte and then restart?
     // InitESPNow();
     // or Simply Restart
@@ -85,11 +87,11 @@ void ScanForSlave() {
   bool slaveFound = 0;
   memset(&slave, 0, sizeof(slave));
 
-  Serial.println("");
+  //Serial.println("");
   if (scanResults == 0) {
-    Serial.println("No WiFi devices in AP Mode found");
+    //Serial.println("No WiFi devices in AP Mode found");
   } else {
-    Serial.print("Found "); Serial.print(scanResults); Serial.println(" devices ");
+    //Serial.print("Found "); Serial.print(scanResults); Serial.println(" devices ");
     for (int i = 0; i < scanResults; ++i) {
       // Print SSID and RSSI for each device found
       String SSID = WiFi.SSID(i);
@@ -97,20 +99,20 @@ void ScanForSlave() {
       String BSSIDstr = WiFi.BSSIDstr(i);
 
       if (PRINTSCANRESULTS) {
-        Serial.print(i + 1);
-        Serial.print(": ");
-        Serial.print(SSID);
-        Serial.print(" (");
-        Serial.print(RSSI);
-        Serial.print(")");
-        Serial.println("");
+        //Serial.print(i + 1);
+        //Serial.print(": ");
+        //Serial.print(SSID);
+        //Serial.print(" (");
+        //Serial.print(RSSI);
+        //Serial.print(")");
+        //Serial.println("");
       }
       delay(10);
       // Check if the current device starts with `Slave`
       if (SSID.indexOf("Slave") == 0) {
         // SSID of interest
-        Serial.println("Found a Slave.");
-        Serial.print(i + 1); Serial.print(": "); Serial.print(SSID); Serial.print(" ["); Serial.print(BSSIDstr); Serial.print("]"); Serial.print(" ("); Serial.print(RSSI); Serial.print(")"); Serial.println("");
+       //Serial.println("Found a Slave.");
+      //Serial.print(i + 1); Serial.print(": "); Serial.print(SSID); Serial.print(" ["); Serial.print(BSSIDstr); Serial.print("]"); Serial.print(" ("); Serial.print(RSSI); Serial.print(")"); Serial.println("");
         // Get BSSID => Mac Address of the Slave
         int mac[6];
         if ( 6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x",  &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5] ) ) {
@@ -131,7 +133,7 @@ void ScanForSlave() {
   }
 
   if (slaveFound) {
-    Serial.println("Slave Found, processing..");
+    //Serial.println("Slave Found, processing..");
   } else {
     Serial.println("Slave Not Found, trying again.");
   }
@@ -148,23 +150,23 @@ bool manageSlave() {
       deletePeer();
     }
 
-    Serial.print("Slave Status: ");
+   //Serial.print("Slave Status: ");
     // check if the peer exists
     bool exists = esp_now_is_peer_exist(slave.peer_addr);
     if ( exists) {
       // Slave already paired.
-      Serial.println("Already Paired");
+  //    Serial.println("Already Paired");
       return true;
     } else {
       // Slave not paired, attempt pair
       esp_err_t addStatus = esp_now_add_peer(&slave);
       if (addStatus == ESP_OK) {
         // Pair success
-        Serial.println("Pair success");
+        //Serial.println("Pair success");
         return true;
       } else if (addStatus == ESP_ERR_ESPNOW_NOT_INIT) {
         // How did we get so far!!
-        Serial.println("ESPNOW Not Init");
+        //Serial.println("ESPNOW Not Init");
         return false;
       } else if (addStatus == ESP_ERR_ESPNOW_ARG) {
         Serial.println("Invalid Argument");
@@ -213,16 +215,28 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   char macStr[18];
   snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
            mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  Serial.print("Last Packet Sent to: "); Serial.println(macStr);
-  Serial.print("Last Packet Send Status: "); Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+//  Serial.print("Last Packet Sent to: "); Serial.println(macStr);
+ // Serial.print("Last Packet Send Status: "); Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
-
+double tans(double a, double b)
+{
+  if(a>=0 && b>=0)
+  return atan(a/b);
+  else if(a<0 && b>=0)
+  return 2*M_PI + atan(a/b);
+  else return M_PI + atan(a/b);
+}
 void setup() {
   Serial.begin(115200);
-  delay(500);
-  for (i=0;i<4;i++){
+  
+  for (i=0;i<6;i++){
     pinMode(buttons[i],INPUT_PULLUP);
   }
+  for (i=0;i<2;i++){
+    pinMode(but_mode[i],INPUT_PULLUP);
+     }
+     pinMode(36,INPUT);
+     pinMode(39,INPUT);
   //Set device in STA mode to begin with
   WiFi.mode(WIFI_STA);
   esp_wifi_set_channel(CHANNEL, WIFI_SECOND_CHAN_NONE);
@@ -247,8 +261,13 @@ void loop() {
     // Add slave as peer if it has not been added already
     bool isPaired = manageSlave();
     if (isPaired) {
+      if(digitalRead(but_mode[0]) == 0){
       sendControls();
-     // joystick();
+      }
+      else if(digitalRead(but_mode[1]) == 0){
+       //Serial.println("Joystick mode is on");
+       delay(100);
+     joystick();}
     } else {
       // slave pair failed
       Serial.println("Slave pair failed!");
@@ -263,11 +282,14 @@ delay(10);
 }
 void sendControls(){
   const uint8_t *peer_addr = slave.peer_addr;
-  but_values.mode = 0;
-  for (i=0;i<4;i++){
+  but_values.con_mode = 0;
+  for (i=0;i<6;i++){
+    int val = digitalRead(buttons[i]);
     Serial.println(digitalRead(buttons[i]));
-    if(digitalRead(buttons[i]) == LOW){
+ 
+    if(val == 0){
      but_values.buton = i+1;
+     break;
     }
     else {
       but_values.buton = 100;
@@ -277,41 +299,90 @@ Serial.println(but_values.buton);
  esp_err_t result = esp_now_send(peer_addr, (uint8_t *) &but_values, sizeof(but_values));
    
   if (result == ESP_OK) {
-    Serial.println("Sent with success");
+  //  Serial.println("Sent with success");
   }
   else {
     Serial.println("Error sending the data");
   }
   
   }
+  
   void joystick()
     {
   const uint8_t *peer_addr = slave.peer_addr;
-  
-  x_joy = analogRead(joy_x);
-  x_joy = map(x_joy, 0, 1023, -100, 100);
-  y_joy = analogRead(joy_y);
-  y_joy = map(y_joy, 0, 1023, -100, 100);
-  Serial.print(x_joy);Serial.println(y_joy);
-  float ang = atan(joy_x/joy_y);
-  vel_x = speedd*cos(ang);
-  vel_y = speedd*sin(ang);
+  double jx = analogRead(39);
+  double jy = analogRead(36);
+  x = map(jx, 0, 4095, -250, 250);//+3*(100-abs(x))/100;
+  y = map(jy, 0, 4095, -250, 250);//-1*(100-abs(y))/100;
 
-  for(i=0;i<3;i++)
+  //Serial.print("X:coordinate ");
+  Serial.print(x);
+  Serial.print("    ");
+ // Serial.print("Y:coordinate ");
+  Serial.print(y);
+  Serial.print("    ");
+  double dist = sqrt(x*x + y*y);
+  speedd = map(dist, 0, 141.4, 0, 100);
+  if(x == 0 || y ==0){
+   for(int i=0;i<4;i++)
   {
-    vel[i] = vel_x*cos(wheel_angle[i] * M_PI/180) + vel_y*sin(wheel_angle[i] * M_PI/180);
+    vel[i]=0;
+
+  } 
+   vel_x = 0;
+   vel_y = 0;
   }
- 
+  else{
+ double dist = sqrt(x * x + y * y);
+  speedd = map(dist, 0, 141.4, 0, 100);
+  double ang = tans(y, x) + M_PI / 2;
+  ang  = (ang*180)/M_PI;
+
+  if (ang >= 350 && ang <= 370){ang = 360;}
+  if(ang>=170 && ang <= 200){ ang =180;}
+  if(ang>=260 && ang<=280) {ang = 270;}
+  if(ang >=360){
+    ang = ang -360;
+  }
+  Serial.print("Angle : ");
+  Serial.println(ang);  
+  double vel_x = speedd * sin((ang*M_PI)/180);
+  double vel_y = speedd * cos((ang*M_PI)/180);
+  Serial.println(vel_x);
+  Serial.println(vel_y);
+  for (int i = 0; i < 4; i++)
+  {
+    vel[i] = vel_x * cos(wheel_angles[i] * M_PI / 180) + vel_y * sin(wheel_angles[i] * M_PI / 180);
+    vel[i] = constrain(vel[i],-50,50);
+    if(vel[i] <= 25 && vel[i]>= -25){
+      vel[i] = 0;
+    }
+  }
   joy_values.w0 = vel[0];
   joy_values.w1 = vel[1];
   joy_values.w2 = vel[2];
-  joy_values.mode = 1;
+  joy_values.w3 = vel[3];
+  joy_values.con_mode = 1;
+ for(int i=0; i<4; i++)
+ {
+  Serial.print(vel[i]);
+  Serial.print("    ");
+ }
+ Serial.println("");
+  Serial.println(vel_x);
+  Serial.println(vel_y);
+  Serial.println(joy_values.con_mode);
+  Serial.println(joy_values.w0);
+  Serial.println(joy_values.w1);
+  Serial.println(joy_values.w2);  
+  Serial.println(joy_values.w3);
   esp_err_t result = esp_now_send(peer_addr, (uint8_t *) &joy_values, sizeof(joy_values));
     if (result == ESP_OK) {
-    Serial.println("Sent with success");
+   //Serial.println("Sent with success");
   }
   else {
     Serial.println("Error sending the data");
+  }
   }
   }
   
